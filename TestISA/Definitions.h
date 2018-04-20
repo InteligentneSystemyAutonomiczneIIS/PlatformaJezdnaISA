bf65438365ffa126a2510a6735f1579a15ab1a12
Artur Mokrosiński <mokrosinski95@gmail.com>
2018-04-20 10:46:22 +0200

    init files

---
+++ TestISA/Definitions.h
@@ -0,0 +1,36 @@
+//INCLUDES
+
+
+
+
+
+//ENGINE PIN
+
+#define A_ENABLE 2  //PWM
+#define A_PHASE 48  //0 - przod, 1 - tyl DIGITAL
+#define B_ENABLE 3  //PWM
+#define B_PHASE 46  //0 - przod, 1 - tyl DIGITAL
+#define MODE 44     //sterowanie 0 - ciężkie, 1 - uproszczone DIGITAL
+
+#define LEFT_ENCODER 40
+#define RIGHT_ENCODER 42
+
+
+//speeds
+
+#define STOP 0
+#define SPEED_0 0
+#define SPEED_1 100
+#define SPEED_2 150
+#define SPEED_3 200
+
+//directions
+
+#define LEFT_FRONT LOW
+#define RIGHT_FRONT HIGH
+#define LEFT_BACK HIGH
+#define RIGHT_BACK LOW
+
+//SERVO
+#define SERVO_X 8
+#define SERVO_Y 9
---
+++ TestISA/TestISA.ino
@@ -0,0 +1,16 @@
+#include "Definitions.h"
+#include "Engine.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  Engine();
+  MovementDirection(0,0);
+  delay(2000);
+  MovementDirection(1,0);
+  MovementSpeed(0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
